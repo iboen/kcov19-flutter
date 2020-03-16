11d369c1346d05d4320004820014c83f7859bb76
Ibnu S Wardy (Ibun) <punyaibun@gmail.com>
2020-03-16 03:23:48 +0000

    Resolve One Signal

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kawalcovid19/blocs/post/bloc.dart';
 import 'package:kawalcovid19/common/color_palettes.dart';
+import 'package:kawalcovid19/common/firebase_notification_handler.dart';
 import 'package:kawalcovid19/const/app_constant.dart';
 import 'package:kawalcovid19/network/repository/kcov_repository.dart';
 import 'package:kawalcovid19/ui/about/about_page.dart';
-import 'package:kawalcovid19/common/firebase_notification_handler.dart';
+// import 'package:kawalcovid19/common/firebase_notification_handler.dart';
 import 'package:kawalcovid19/ui/dashboard/dashboard_page.dart';
 import 'package:kawalcovid19/blocs/SimpleBlocDelegate.dart';
 import 'package:kawalcovid19/ui/home/detail_article.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   // BlocSupervisor oversees Blocs and delegates to BlocDelegate.
@@ -38,8 +40,58 @@ class _MyAppState extends State<MyApp> {
           isDark ? ColorPalettes.darkPrimary : ColorPalettes.lightPrimary,
       statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
     ));
-    // Check the default of FCM
-    new FirebaseNotifications().setUpFirebase();
+    
+    //Setup One Signal
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    if (!mounted) return;
+
+    OneSignal.shared.setLogLevel(OSLogLevel.none, OSLogLevel.none);
+
+    var settings = {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.promptBeforeOpeningPushUrl: true
+    };
+
+    OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) {
+      print("Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}");
+    });
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+          print("Opened notification: \n${result.notification.jsonRepresentation().replaceAll("\\n", "\n")}");
+    });
+
+    OneSignal.shared
+    .setInAppMessageClickedHandler((OSInAppMessageAction action) {
+      print( "In App Message Clicked: \n${action.jsonRepresentation().replaceAll("\\n", "\n")}");
+    });
+
+    OneSignal.shared
+        .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+      print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    bool requiresConsent = await OneSignal.shared.requiresUserPrivacyConsent();
+     print("REQUIRES CONSENT $requiresConsent");
+
+
+    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+      print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setEmailSubscriptionObserver(
+        (OSEmailSubscriptionStateChanges changes) {
+      print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+    });
+
+    // NOTE: Replace with your own app ID from https://www.onesignal.com
+    await OneSignal.shared.init("df1b119e-14c3-4361-8a97-76ae9a827bf6", iOSSettings: settings);
+
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
 
   }
 
